81a23aa4c81740c5fbfe5db1501a96aca371d5e8
Allan Moya <78510463+allan-moya@users.noreply.github.com>
2022-01-28 19:24:00 -0800

    third commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,15 +1,14 @@
-import axios from 'axios'
-export const baseUrl = 'https://bayut.p.rapidapi.com'
+import axios from "axios";
 
-
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'ad4cfc3d0dmshe6123a56319ba72p1a306cjsnc87dd408886d'
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
         },
     });
-    return data;
 
+    return data;
 }
